7301954513d340e7bcd1a26c2d434b5ca4c95802
georgemmp <machadogeorge.p@gmail.com>
2019-08-16 11:52:30 -0300

    Implementando chamada de api

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-export default class Main extends Component {
-    static navigationOptions = {
-        title: 'JSHunt'
-    };
+import Api from '../services/api';
 
-    render() {
-        return (
-            <View>
-                <Text>Página Main</Text>
-            </View>
-        );
-    }
+export default class Main extends Component {
+  static navigationOptions = {
+    title: 'JSHunt'
+  };
+
+  state = {
+    docs: []
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await Api.get('/products');
+    const { docs } = response.data;
+    console.log(docs);
+    this.setState({ docs });
+  };
+
+  render() {
+    const { docs } = this.state;
+    return (
+      <View>
+        <Text>Pagina Main</Text>
+        {docs.map(items => (
+          <Text key={items._id}>{items.title}</Text>
+        ))}
+      </View>
+    );
+  }
 }
